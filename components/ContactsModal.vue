7d799116555b0712583551ffa651454a04a1cbd4
Andrey Khataev <andrey.hataev@balance-pl.ru>
2020-01-10 09:06:33 +0300

    Edit contacts

--- components/ContactsModal.vue
+++ components/ContactsModal.vue
@@ -1,56 +1,30 @@
 <template>
-  <b-modal
-    id="contactsModal"
-    size="lg"
-    title="Я в сети"
-    ok-title="Закрыть"
-    ok-only
-  >
+  <b-modal id="contactsModal" size="lg" title="Я в сети" ok-title="Закрыть" ok-only>
     <ul>
       <li>
         <p>
-          В чате
-          <a target="_blank" href="https://gitter.im/micexopen/Lobby">Gitter</a>
-          <li>
-            <p>
-              На форуме
-              <a
-                target="_blank"
-                href="http://www.banki.ru/profile/?action=profile&UID=823120"
-                >banki.ru</a
-              >
-            </p>
-          </li>
-          <li>
-            <p>
-              На форуме
-              <a
-                target="_blank"
-                href="http://octavtrading.forum.wtf/messages.php?action=new&uid=69"
-                >octavtrading.forum.wtf</a
-              >
-            </p>
-          </li>
-          <li>
-            <p>
-              <a target="_blank" href="https://vk.com/khataev">Вконтакте</a>
-            </p>
-          </li>
-          <li>
-            <p>
-              <a
-                target="_blank"
-                href="https://www.facebook.com/profile.php?id=100006483577333"
-                >Facebook</a
-              >
-            </p>
-          </li>
-          <li>
-            <p>
-              Или просто напишите мне на
-              <a target="_blank" href="mailto:admin@khataev.com">почту</a>
-            </p>
-          </li>
+          Написать в
+          <a
+            target="_blank"
+            href="https://tele.click/joinchat/CoDJIg5MEUdaHiQSVFCs1Q"
+          >телеграм чат</a> или
+          <a target="_blank" href="https://tele.click/khataev">напрямую</a>
+        </p>
+      </li>
+      <li>
+        <p>
+          <a target="_blank" href="https://vk.com/khataev">Вконтакте</a>
+        </p>
+      </li>
+      <li>
+        <p>
+          <a target="_blank" href="https://www.facebook.com/profile.php?id=100006483577333">Facebook</a>
+        </p>
+      </li>
+      <li>
+        <p>
+          Или просто напишите мне на
+          <a target="_blank" href="mailto:admin@khataev.com">почту</a>
         </p>
       </li>
     </ul>
